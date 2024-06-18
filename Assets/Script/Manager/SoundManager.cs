86d3f744be246d24f1e3290923d8d9047f02c113
O Young Hwang <poas753@naver.com>
2024-06-18 09:11:13 +0900

    Sound

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField][Range(0f, 1f)] private float soundEffectVolume;
+    [SerializeField][Range(0f, 1f)] private float soundEffectPitchVariance;
+    [SerializeField][Range(0f, 1f)] private float musicVolume;
+
+    private AudioSource musicAudioSource;
+    public AudioClip musicClip;
+
+    private void Awake()
+    {
+        instance = this;
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.volume = musicVolume;
+        musicAudioSource.loop = true;
+    }
+
+    private void Start()
+    {
+        ChangeBackGroundMusic(musicClip);
+    }
+    public static void ChangeBackGroundMusic(AudioClip music)
+    {
+        instance.musicAudioSource.Stop();
+        instance.musicAudioSource.clip = music;
+        instance.musicAudioSource.Play();
+    }
+}
